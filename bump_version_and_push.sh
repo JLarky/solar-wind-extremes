4718326ec67bd751965b7cb6cb328bd3ab9864b9
JLarky <jlarky@gmail.com>
2012-01-06 18:54:40 +0400

    fix script

--- bump_version_and_push.sh
+++ bump_version_and_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./update_cache.sh
-git commit cache.manifest cache.manifest.src -m 'bump cache.manifest version'
+git commit index.html appcache.manifest appcache.manifest.src -m 'bump version'
 git push
