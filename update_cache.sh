4dbdadc9acd07ccd189fd016cbc75d685db1b369
JLarky <jlarky@gmail.com>
2012-01-06 18:04:11 +0400

    update html

--- update_cache.sh
+++ update_cache.sh
@@ -8,3 +8,5 @@ echo "#images"
 find images/ -type f) >> appcache.manifest
 
 (echo '2c'; echo \# `date; git log -1 --oneline;`; echo '.'; echo 'wq') | ed -s appcache.manifest
+
+(echo '2c'; echo '<!-- last updated:' `date` '-->'; echo '.'; echo 'wq') | ed -s index.html
